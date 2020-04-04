e6824c8d67eab79fcb42ee0b06ec18e4bdc871f4
Yuriy Lazarev <unisay@users.noreply.github.com>
2020-04-04 09:55:45 +0200

    First release

--- src/Parse/Dont/Validate/Parser.elm
+++ src/Parse/Dont/Validate/Parser.elm
@@ -1,36 +1,43 @@
 module Parse.Dont.Validate.Parser exposing
-    ( Parser
-    , all
-    , andThen
-    , by
-    , contramap
-    , fail
-    , fromReader
-    , map
-    , mapError
-    , merge
-    , ok
-    , run
-    , succeed
-    , validateBy
-    , fromPredicate
-    , zip
-    , note
+    ( Parser, all, andThen, by, contramap, fail, fromReader, map, mapError
+    , merge, ok, run, succeed, validateBy, fromPredicate, zip, note
     )
 
--- https://lexi-lambda.github.io/blog/2019/11/05/parse-don-t-validateBy/
+{-| This library provides a bunch of parser combinators
+and could be used for Web form validation.
+
+It follows ideas described here:
+<https://lexi-lambda.github.io/blog/2019/11/05/parse-don-t-validateBy/>
+
+@docs Parser, all, andThen, by, contramap, fail, fromReader, map, mapError
+@docs merge, ok, run, succeed, validateBy, fromPredicate, zip, note
+
+-}
 
 import Cons as Cons exposing (Cons)
 
+
+{-| Parser type
+
+inp Input type (what is parsed/validated)
+err Error type (Could be your custom error type)
+out Output type (Could be your domain type)
+
+-}
 type alias Parser inp err out =
     inp -> Result (Cons err) out
 
 
+{-| Create Parser from function of type `inp -> Maybe out`
+-}
 fromReader : (inp -> Maybe out) -> err -> Parser inp err out
 fromReader reader error =
     reader >> Result.fromMaybe (Cons.singleton error)
 
 
+{-| Create Parser from predicate function that returns a wrapped input value.
+Parser fails with provided error in case if predicate evaluates to False.
+-}
 fromPredicate : (inp -> Bool) -> err -> (inp -> out) -> Parser inp err out
 fromPredicate predicate err constructor inp =
     if predicate inp then
@@ -62,28 +69,45 @@ dimap before =
     trimap before identity
 
 
+{-| Maps over parser result
+-}
 map : (out0 -> out1) -> Parser inp err out0 -> Parser inp err out1
 map =
     dimap identity
 
-note : err -> Parser inp () out -> Parser inp err out
-note  = mapError << always 
 
+{-| Given a parser without error decorates it with an error provided
+-}
+note : err -> Parser inp () out -> Parser inp err out
+note =
+    mapError << always
+
+
+{-| Maps over parser error
+-}
 mapError : (err0 -> err1) -> Parser inp err0 out -> Parser inp err1 out
 mapError f =
     trimap identity f identity
 
 
+{-| Maps over Parser input
+-}
 contramap : (inp1 -> inp0) -> Parser inp0 err out -> Parser inp1 err out
 contramap f =
     dimap f identity
 
 
+{-| Parser composition: result produced by second parser is fed to the first
+-}
 andThen : Parser a err b -> Parser inp err a -> Parser inp err b
 andThen f p inp =
     Result.andThen f (p inp)
 
 
+{-| When given a function that merges parsing results
+and two parsers producing such results - returns a parser
+that produces a result of that function.
+-}
 merge : (a -> b -> c) -> Parser inp err a -> Parser inp err b -> Parser inp err c
 merge join pa pb inp =
     case pa inp of
@@ -104,11 +128,18 @@ merge join pa pb inp =
                     Ok (join resA resB)
 
 
+{-| Given 2 parsers, feeds same input into both of them
+and returns results as tuple
+-}
 zip : Parser inp err a -> Parser inp err b -> Parser inp err ( a, b )
 zip =
     merge Tuple.pair
 
 
+{-| Given a non-empty list of uniform parsers returns a parser
+that produces a non empty list of their results.
+If any of the provided parsers fails - resulting parser also fails.
+-}
 all : Cons (Parser inp err a) -> Parser inp err (Cons a)
 all parsers inp =
     let
@@ -134,6 +165,15 @@ all parsers inp =
         (Cons.map (\p -> Result.map Cons.singleton (p inp)) parsers)
 
 
+{-| Useful for a "pipeline" style:
+
+    example : Parser { bar : String, baz : String } Error Foo
+    example =
+        succeed (\a b -> Foo a b)
+            |> by .bar parseBar
+            |> by .baz parseBaz
+
+-}
 by :
     (inp1 -> inp0)
     -> Parser inp0 err out
@@ -143,6 +183,23 @@ by f p =
     merge (|>) (contramap f p)
 
 
+{-| Useful for a "pipeline" style when validator emits an error message but
+reurns () in order not to consume constructor argument:
+
+    example : Parser { bar: String, baz: String } Error Foo
+    example =
+        succeed (\bar baz -> Foo bar baz)
+            |> by .bar parseBar
+            |> by .baz parseBaz
+            |> validateBy identity barBazParser
+
+    barBazParser : { bar: String, baz: String } Error Unit
+    barBazParser { bar, baz } =
+        if bar /= baz
+            then Parser.fail "Bar != Baz"
+            else Parser.ok ()
+
+-}
 validateBy :
     (inp1 -> inp0)
     -> Parser inp0 err ()
@@ -152,21 +209,37 @@ validateBy f p =
     by f p << map always
 
 
+{-| Useful for a "pipeline" style:
+
+    example : Parser { bar : String, baz : String } Error Foo
+    example =
+        succeed (\a b -> Foo a b)
+            |> by .bar parseBar
+            |> by .baz parseBaz
+            |> validateBy identity barBazParser
+
+-}
 succeed : out -> Parser input err out
 succeed =
     always << Ok
 
 
+{-| Returns successfully parsed value
+-}
 ok : out -> Result (Cons err) out
 ok =
     Ok
 
 
+{-| Returns error
+-}
 fail : err -> Result (Cons err) out
 fail =
     Err << Cons.singleton
 
 
+{-| Syntactic abstraction: applies parser to its input
+-}
 run : Parser input err out -> input -> Result (Cons err) out
 run =
     identity
--- src/Parse/Dont/Validate/String.elm
+++ src/Parse/Dont/Validate/String.elm
@@ -1,28 +1,46 @@
 module Parse.Dont.Validate.String exposing
-    ( float
-    , int
-    , nonEmpty
+    ( nonEmpty
     , uncons
+    , int
+    , float
     )
+
+{-| Set of primitive string parsers
+
+@docs nonEmpty
+@docs uncons
+@docs int
+@docs float
+
+-}
 
 import Parse.Dont.Validate.Parser as Parser exposing (Parser)
 
 
+{-| Parse a string returning unit as an error if input is empty
+-}
 nonEmpty : Parser String () String
 nonEmpty =
     Parser.fromPredicate (not << String.isEmpty) () identity
 
 
+{-| Parse a string returning unit as an error if input is empty
+or tuple of first character and the rest of the string otherwise.
+-}
 uncons : Parser String () ( Char, String )
 uncons =
     Parser.fromReader String.uncons ()
 
 
+{-| Parse a string returning unit as an error or Int as a result.
+-}
 int : Parser String () Int
 int =
     Parser.fromReader String.toInt ()
 
 
+{-| Parse a string returning unit as an error or Float as a result.
+-}
 float : Parser String () Float
 float =
     Parser.fromReader String.toFloat ()
